5f52a1fef943eacb80752741b9ddc3595e86401f
jghoover <30665181+jghoover@users.noreply.github.com>
2017-08-28 07:03:00 -0500

    Project Template

---
+++ esp.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class esp 
+{
+	
+	static int gradesMin(int [] grades)
+	{
+		
+	}
+	
+	static int gradesMax(int [] grades)
+	{
+		
+	}
+	
+	static int gradesMean(int [] grades)
+	{
+		
+	}
+	
+	static int gradesMedian(int [] grades)
+	{
+		
+	}
+
+	static int [] gradesDistribution(int [] grades)
+	{
+		
+	}
+	
+	//extra credit---remove this function if you don't do the EC
+	static int gradesStandardDev(int [] grades)
+	{
+		
+	}
+	
+	/* you will need to fill in the arguments for this function.
+	 * the arguments should be the various statistics you computed
+	 * and should print them out in a neatly-formatted manner.
+	 */
+	static void printGradesInfo()
+	{
+		
+	}	
+	
+	public static void main(String[] args) throws IOException
+	{
+		
+	}
+
+}
